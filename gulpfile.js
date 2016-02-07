fcd079ed82e13b6854784f7ec29430f6a8b0f58d
Tomasz Guzialek <tomasz@guzialek.info>
2016-02-07 20:32:36 +0100

    Using hoodie instead of hoodie-server directly.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 
 gulp.task('hoodie', function() {
-    var child = exec('node ./node_modules/hoodie-server/bin/start --www . --custom-ports 6001,6002,6003');
+    var child = exec('npm start -- --admin-password hoodie --www . --port 6001 --admin-port 6002 --db-port 6003');
       child.stdout.on('data', function(data) {
           console.log(data);
       });
